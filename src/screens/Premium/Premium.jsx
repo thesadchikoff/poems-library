6be90da681ae7cb5e60aba1bbf30d11b4753a922
bombasters <59049797+bombasters@users.noreply.github.com>
2023-01-24 03:23:04 +0300

    Add files via upload

--- src/screens/Premium/Premium.jsx
+++ src/screens/Premium/Premium.jsx
@@ -1,13 +1,8 @@
 
-import React, {useEffect} from 'react';
+import React from 'react';
 import styles from './Premium.module.scss'
-import {seo} from "../../helpers/seo.js";
+
 const Premium = () => {
-    useEffect(() => {
-        seo({
-            title: 'Премиум подписка',
-        })
-    }, [])
     return (
         <div className={styles.wrapper}>
             <div className={`${styles.premiumInfoBlock} desktop:w-[600px] w-full`}>
